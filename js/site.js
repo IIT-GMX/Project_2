9db08e0960eeda37b6510eda53ab078bc247f674
gtoledorodriguez <gtoledo550@gmail.com>
2022-12-01 23:54:50 -0600

    changed length of time image rotates

--- js/site.js
+++ js/site.js
@@ -20,7 +20,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
 
 function checkForm() {
